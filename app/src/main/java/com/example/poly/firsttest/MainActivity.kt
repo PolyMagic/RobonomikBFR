130bdfbeed5391d0533cf111f60d73007ae8aa9b
PolyMagic <bartek.marynczak@op.pl>
2018-11-24 19:27:52 +0100

    Cleaning

--- app/src/main/java/com/example/poly/firsttest/MainActivity.kt
+++ app/src/main/java/com/example/poly/firsttest/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.poly.firsttest
 
 import android.Manifest
-import android.app.Activity
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.os.Build
+import android.net.Uri
+
 import android.os.Bundle
-import android.os.VibrationEffect
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.ActivityCompat
 import android.view.View
@@ -24,144 +22,78 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-import android.os.Vibrator
 
-import android.widget.TextView
-import java.io.InputStream
-import java.io.OutputStream
+
+import kotlinx.android.synthetic.main.content_main.text02
+import kotlinx.android.synthetic.main.app_bar_main.send_button
+import kotlinx.android.synthetic.main.content_main.map_link
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val mContext = this@MainActivity
 
-    private var outputStream: OutputStream? = null
-    private var inStream: InputStream? = null
-
-    internal var locationListener: LocationListener = object : LocationListener {
+    var locationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
-
             val lat = location.latitude
             val lon = location.longitude
 
-            val test = findViewById<View>(R.id.text02) as TextView
+            val test = convert(lat,lon)
 
-            test.text = "" + lat + "\n" + lon + "\n" + "https://www.google.pl/maps/@" + lat + "," + lon + ",17.5z"
+            text02.text = test
+            map_link.text = "https://www.google.pl/maps/place/" + test
         }
 
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
-
-        }
-
-        override fun onProviderEnabled(provider: String) {
-            val test = findViewById<View>(R.id.text02) as TextView
-            test.text = "Called onProviderEnabled"
-        }
-
-        override fun onProviderDisabled(provider: String) {
-
-        }
+        override fun onProviderEnabled(provider: String) {}
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create Toolbar
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-
-        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
-
-
-        // GPS Start Listener
-        fab.setOnClickListener { ActivityCompat.requestPermissions(mContext as Activity, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), 112) }
-
-
+        // Left Side Drawer
         val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
-        val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
 
         val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
         navigationView.setNavigationItemSelectedListener(this)
 
-        val panic = findViewById<View>(R.id.panic_button) as FloatingActionButton
-        val send = findViewById<View>(R.id.search_button) as FloatingActionButton
 
-        startBt()
 
-        panic.setOnClickListener {
-//            val v = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-//
-//
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
-//            }
-//            else {
-//                v.vibrate(500)
-//            }
-//
-            run()
-        }
-
-        send.setOnClickListener {
-            write("Hym... działa?")
+        // Open Google Maps Link
+        send_button.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.setData(Uri.parse(map_link.text.toString()))
+            startActivity(i)
         }
 
 
-}
-
-    fun startBt(){
-        val ba = BluetoothAdapter.getDefaultAdapter() as BluetoothAdapter
-
-        if (ba.isEnabled()) {
-            val bondedDevices = ba.getBondedDevices()
-
-            if(bondedDevices.size > 0) {
-                val devices = bondedDevices.toTypedArray()
-                val device = devices[0]
-                val uuids = device.getUuids()
-                val socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid())
-                socket.connect();
-                outputStream = socket.getOutputStream();
-                inStream = socket.getInputStream();
-            }
-
-
+        val gpsStartBtn = findViewById<View>(R.id.fab) as FloatingActionButton
+        // GPS Request Permissions
+        gpsStartBtn.setOnClickListener {
+            val permList = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(mContext, permList , 112)
         }
+
+
     }
-
-    fun write(s: String) {
-        outputStream?.write(s.toByteArray())
-    }
-
-    fun run() {
-        val BUFFER_SIZE = 1024
-        val buffer = ByteArray(BUFFER_SIZE)
-        var bytes = 0
-        val b = BUFFER_SIZE
-
-        while (true) {
-
-            bytes = inStream!!.read(buffer, bytes, BUFFER_SIZE - bytes)
-
-            println("=P= ${bytes.toString()}")
-        }
-    }
-
 
     override fun onRequestPermissionsResult(requestCode: Int, permisions: Array<String>, grantResults: IntArray) {
-
         val mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0f, locationListener)
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0f, locationListener)
 
-            val test = findViewById<View>(R.id.text02) as TextView
-            test.text = "Listening..."
+            text02.text = "GPS Listening..."
 
         }
-
     }
 
     override fun onBackPressed() {
@@ -174,15 +106,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // adds items to the action bar
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         val id = item.itemId
-
         return if (id == R.id.action_settings) {
             true
         } else super.onOptionsItemSelected(item)
@@ -192,12 +121,55 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
-        //        if (id == R.id.nav_camera) {
-        //            // Handle the camera action
-        //        }
+        if (id == R.id.home) { }
 
         val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
         drawer.closeDrawer(GravityCompat.START)
         return true
+    }
+
+
+
+    // Utils
+
+    // Lat Long to cordinates converter for Google Maps
+    private fun convert(latitude: Double, longitude: Double): String {
+        val builder = StringBuilder()
+
+
+        val latitudeDegrees = Location.convert(Math.abs(latitude), Location.FORMAT_SECONDS)
+        val latitudeSplit = latitudeDegrees.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        builder.append(latitudeSplit[0])
+        builder.append("°")
+        builder.append(latitudeSplit[1])
+        builder.append("'")
+        builder.append(latitudeSplit[2])
+        builder.append("\"")
+        if (latitude < 0) {
+            builder.append("S ")
+        } else {
+            builder.append("N ")
+        }
+
+
+        builder.append(" ")
+
+
+
+        val longitudeDegrees = Location.convert(Math.abs(longitude), Location.FORMAT_SECONDS)
+        val longitudeSplit = longitudeDegrees.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        builder.append(longitudeSplit[0])
+        builder.append("°")
+        builder.append(longitudeSplit[1])
+        builder.append("'")
+        builder.append(longitudeSplit[2])
+        builder.append("\"")
+        if (longitude < 0) {
+            builder.append("W ")
+        } else {
+            builder.append("E ")
+        }
+
+        return builder.toString()
     }
 }
